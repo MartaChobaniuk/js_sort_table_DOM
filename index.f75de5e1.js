6a32bd5b01c068f478e41e5fec8ff706b9b90106
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-27 19:53:56 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-shadow */ "use strict";
+const people = [
+    ...document.querySelectorAll("tbody tr")
+];
+const heading = document.querySelector("thead tr");
+heading.addEventListener("click", (event)=>{
+    const index = [
+        ...heading.children
+    ].indexOf(event.target);
+    const sorted = people.sort((a, b)=>{
+        const first = a.children[index].innerText.replace(/[$,]/g, "");
+        const second = b.children[index].innerText.replace(/[$,]/g, "");
+        if (isNaN(+first)) return first.localeCompare(second);
+        return first - second;
+    });
+    document.querySelector("tbody").append(...sorted);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
